fe1302ec92a1d736f2644e17d5cfa113693e310d
WiTace <as.watcharich_st@tni.ac.th>
2017-02-03 02:01:02 +0000

    First version

---
+++ src/generate_list.py
@@ -0,0 +1,18 @@
+import random
+def generate_list():
+        alist = [x for x in range(random.radint(-10,10))]
+        return alist
+    """
+    git print(generate_list)
+    """
+def printIt()
+    print(generate_list())
+    
+def main():
+    printIT()
+    """
+    If this scrit file is called, it will run main() directly
+    """
+    if _name_=='_main_':
+        print("Test printIt():")
+        main()
